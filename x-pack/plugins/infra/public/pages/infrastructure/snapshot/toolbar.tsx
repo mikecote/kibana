451d883dcfcb6f4cdd91c5c095833dd7410c10c2
Mike Cote <mikecote@users.noreply.github.com>
2019-04-17 15:50:47 -0400

    Hack in UI

--- x-pack/plugins/infra/public/pages/infrastructure/snapshot/toolbar.tsx
+++ x-pack/plugins/infra/public/pages/infrastructure/snapshot/toolbar.tsx
@@ -4,9 +4,27 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import {
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiButton,
+  EuiModal,
+  EuiOverlayMask,
+  EuiModalHeader,
+  EuiModalHeaderTitle,
+  EuiModalBody,
+  EuiModalFooter,
+  EuiButtonEmpty,
+  EuiForm,
+  EuiFormRow,
+  EuiFieldText,
+  EuiSelect,
+  EuiCheckboxGroup,
+  EuiRange,
+} from '@elastic/eui';
 import { injectI18n } from '@kbn/i18n/react';
 import React from 'react';
+import { kfetch } from 'ui/kfetch';
 
 import { AutocompleteField } from '../../../components/autocomplete_field';
 import { Toolbar } from '../../../components/eui/toolbar';
@@ -21,103 +39,322 @@ import { WithWaffleTime } from '../../../containers/waffle/with_waffle_time';
 import { WithKueryAutocompletion } from '../../../containers/with_kuery_autocompletion';
 import { WithSource } from '../../../containers/with_source';
 
-export const SnapshotToolbar = injectI18n(({ intl }) => (
-  <Toolbar>
-    <EuiFlexGroup alignItems="center" justifyContent="spaceBetween" gutterSize="m">
-      <EuiFlexItem>
-        <WithSource>
-          {({ derivedIndexPattern }) => (
-            <WithKueryAutocompletion indexPattern={derivedIndexPattern}>
-              {({ isLoadingSuggestions, loadSuggestions, suggestions }) => (
-                <WithWaffleFilter indexPattern={derivedIndexPattern}>
-                  {({
-                    applyFilterQueryFromKueryExpression,
-                    filterQueryDraft,
-                    isFilterQueryDraftValid,
-                    setFilterQueryDraftFromKueryExpression,
-                  }) => (
-                    <AutocompleteField
-                      isLoadingSuggestions={isLoadingSuggestions}
-                      isValid={isFilterQueryDraftValid}
-                      loadSuggestions={loadSuggestions}
-                      onChange={setFilterQueryDraftFromKueryExpression}
-                      onSubmit={applyFilterQueryFromKueryExpression}
-                      placeholder={intl.formatMessage({
-                        id: 'xpack.infra.homePage.toolbar.kqlSearchFieldPlaceholder',
-                        defaultMessage: 'Search for infrastructure data… (e.g. host.name:host-1)',
-                      })}
-                      suggestions={suggestions}
-                      value={filterQueryDraft ? filterQueryDraft.expression : ''}
-                    />
+interface State {
+  showModal: boolean;
+  alert?: string;
+  interval: string;
+  throttle?: string;
+  warningThreshold: number;
+  warningActionsMap: {
+    [id: string]: boolean;
+  };
+  warningActions: Array<{
+    id: string;
+    realId: string;
+    label: string;
+  }>;
+  severeThreshold: number;
+  severeActionsMap: {
+    [id: string]: boolean;
+  };
+  severeActions: Array<{
+    id: string;
+    realId: string;
+    label: string;
+  }>;
+}
+
+let counter = 0;
+function makeId() {
+  return (++counter).toString();
+}
+
+class Component extends React.Component<any, State> {
+  constructor(props: any) {
+    super(props);
+
+    const warningIdPrefix = makeId();
+    const severeIdPrefix = makeId();
+
+    this.state = {
+      showModal: false,
+      alert: 'cpu-check',
+      interval: '10000',
+      throttle: undefined,
+      warningThreshold: 50,
+      warningActionsMap: {},
+      warningActions: [
+        {
+          id: `${warningIdPrefix}:console-log`,
+          realId: 'console-log',
+          label: 'Send message to the console',
+        },
+        {
+          id: `${warningIdPrefix}:turn-on-alarm-light`,
+          realId: 'turn-on-alarm-light',
+          label: 'Turn on a physical alarm light',
+        },
+      ],
+      severeThreshold: 80,
+      severeActionsMap: {},
+      severeActions: [
+        {
+          id: `${severeIdPrefix}:console-log`,
+          realId: 'console-log',
+          label: 'Send message to the console',
+        },
+        {
+          id: `${severeIdPrefix}:turn-on-alarm-light`,
+          realId: 'turn-on-alarm-light',
+          label: 'Turn on a physical alarm light',
+        },
+      ],
+    };
+  }
+  async onSaveClick() {
+    const response = await kfetch({
+      method: 'POST',
+      pathname: '/api/schedule-action',
+      body: JSON.stringify({
+        id: this.state.alert,
+        interval: Number(this.state.interval),
+        throttle: this.state.throttle ? Number(this.state.throttle) : undefined,
+        actionGroupsPriority: ['severe', 'warning', 'default'],
+        actionGroups: {
+          default: [],
+          warning: this.state.warningActions
+            .filter(({ id }) => this.state.warningActionsMap[id])
+            .map(action => ({ id: action.realId, params: {} })),
+          severe: this.state.severeActions
+            .filter(({ id }) => this.state.severeActionsMap[id])
+            .map(action => ({ id: action.realId, params: {} })),
+        },
+        checkParams: {
+          warningThreshold: this.state.warningThreshold,
+          severeThreshold: this.state.severeThreshold,
+        },
+      }),
+    });
+    if (response.success === true) {
+      this.setState({ showModal: false });
+    }
+  }
+  render() {
+    const { intl } = this.props;
+    return (
+      <Toolbar>
+        <EuiFlexGroup alignItems="center" justifyContent="spaceBetween" gutterSize="m">
+          <EuiFlexItem>
+            <WithSource>
+              {({ derivedIndexPattern }) => (
+                <WithKueryAutocompletion indexPattern={derivedIndexPattern}>
+                  {({ isLoadingSuggestions, loadSuggestions, suggestions }) => (
+                    <WithWaffleFilter indexPattern={derivedIndexPattern}>
+                      {({
+                        applyFilterQueryFromKueryExpression,
+                        filterQueryDraft,
+                        isFilterQueryDraftValid,
+                        setFilterQueryDraftFromKueryExpression,
+                      }) => (
+                        <AutocompleteField
+                          isLoadingSuggestions={isLoadingSuggestions}
+                          isValid={isFilterQueryDraftValid}
+                          loadSuggestions={loadSuggestions}
+                          onChange={setFilterQueryDraftFromKueryExpression}
+                          onSubmit={applyFilterQueryFromKueryExpression}
+                          placeholder={intl.formatMessage({
+                            id: 'xpack.infra.homePage.toolbar.kqlSearchFieldPlaceholder',
+                            defaultMessage:
+                              'Search for infrastructure data… (e.g. host.name:host-1)',
+                          })}
+                          suggestions={suggestions}
+                          value={filterQueryDraft ? filterQueryDraft.expression : ''}
+                        />
+                      )}
+                    </WithWaffleFilter>
                   )}
-                </WithWaffleFilter>
+                </WithKueryAutocompletion>
               )}
-            </WithKueryAutocompletion>
-          )}
-        </WithSource>
-      </EuiFlexItem>
-      <EuiFlexItem grow={false}>
-        <WithWaffleTime resetOnUnmount>
-          {({ currentTime, isAutoReloading, jumpToTime, startAutoReload, stopAutoReload }) => (
-            <WaffleTimeControls
-              currentTime={currentTime}
-              isLiveStreaming={isAutoReloading}
-              onChangeTime={jumpToTime}
-              startLiveStreaming={startAutoReload}
-              stopLiveStreaming={stopAutoReload}
-            />
-          )}
-        </WithWaffleTime>
-      </EuiFlexItem>
-    </EuiFlexGroup>
-    <EuiFlexGroup alignItems="center" gutterSize="m">
-      <WithSource>
-        {({ derivedIndexPattern }) => (
-          <WithWaffleOptions>
-            {({
-              changeMetric,
-              changeNodeType,
-              changeGroupBy,
-              changeCustomOptions,
-              customOptions,
-              groupBy,
-              metric,
-              nodeType,
-            }) => (
-              <React.Fragment>
-                <EuiFlexItem grow={false}>
-                  <WaffleNodeTypeSwitcher
-                    nodeType={nodeType}
-                    changeNodeType={changeNodeType}
-                    changeMetric={changeMetric}
-                    changeGroupBy={changeGroupBy}
-                  />
-                </EuiFlexItem>
-                <EuiFlexItem grow={false}>
-                  <WaffleMetricControls
-                    metric={metric}
-                    nodeType={nodeType}
-                    onChange={changeMetric}
-                  />
-                </EuiFlexItem>
-                <EuiFlexItem grow={false}>
-                  <WaffleGroupByControls
-                    groupBy={groupBy}
-                    nodeType={nodeType}
-                    onChange={changeGroupBy}
-                    fields={derivedIndexPattern.fields}
-                    onChangeCustomOptions={changeCustomOptions}
-                    customOptions={customOptions}
-                  />
-                </EuiFlexItem>
-                <EuiFlexItem grow={false}>
-                  <SourceConfigurationButton />
-                </EuiFlexItem>
-              </React.Fragment>
+            </WithSource>
+          </EuiFlexItem>
+          <EuiFlexItem grow={false}>
+            <WithWaffleTime resetOnUnmount>
+              {({ currentTime, isAutoReloading, jumpToTime, startAutoReload, stopAutoReload }) => (
+                <WaffleTimeControls
+                  currentTime={currentTime}
+                  isLiveStreaming={isAutoReloading}
+                  onChangeTime={jumpToTime}
+                  startLiveStreaming={startAutoReload}
+                  stopLiveStreaming={stopAutoReload}
+                />
+              )}
+            </WithWaffleTime>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+        <EuiFlexGroup alignItems="center" gutterSize="m">
+          <WithSource>
+            {({ derivedIndexPattern }) => (
+              <WithWaffleOptions>
+                {({
+                  changeMetric,
+                  changeNodeType,
+                  changeGroupBy,
+                  changeCustomOptions,
+                  customOptions,
+                  groupBy,
+                  metric,
+                  nodeType,
+                }) => (
+                  <React.Fragment>
+                    <EuiFlexItem grow={false}>
+                      <WaffleNodeTypeSwitcher
+                        nodeType={nodeType}
+                        changeNodeType={changeNodeType}
+                        changeMetric={changeMetric}
+                        changeGroupBy={changeGroupBy}
+                      />
+                    </EuiFlexItem>
+                    <EuiFlexItem grow={false}>
+                      <WaffleMetricControls
+                        metric={metric}
+                        nodeType={nodeType}
+                        onChange={changeMetric}
+                      />
+                    </EuiFlexItem>
+                    <EuiFlexItem grow={false}>
+                      <WaffleGroupByControls
+                        groupBy={groupBy}
+                        nodeType={nodeType}
+                        onChange={changeGroupBy}
+                        fields={derivedIndexPattern.fields}
+                        onChangeCustomOptions={changeCustomOptions}
+                        customOptions={customOptions}
+                      />
+                    </EuiFlexItem>
+                    <EuiFlexItem grow={false}>
+                      <EuiButton onClick={() => this.setState({ showModal: true })}>
+                        Create Alert
+                      </EuiButton>
+                    </EuiFlexItem>
+                    <EuiFlexItem grow={false}>
+                      <SourceConfigurationButton />
+                    </EuiFlexItem>
+                  </React.Fragment>
+                )}
+              </WithWaffleOptions>
             )}
-          </WithWaffleOptions>
-        )}
-      </WithSource>
-    </EuiFlexGroup>
-  </Toolbar>
-));
+          </WithSource>
+          {this.state.showModal && (
+            <EuiOverlayMask>
+              <EuiModal onClose={() => this.setState({ showModal: false })}>
+                <EuiModalHeader>
+                  <EuiModalHeaderTitle>Create an alert</EuiModalHeaderTitle>
+                </EuiModalHeader>
+                <EuiModalBody>
+                  <EuiForm>
+                    <EuiFormRow label="Alert">
+                      <EuiSelect
+                        hasNoInitialSelection
+                        value={this.state.alert}
+                        options={[{ value: 'cpu-check', text: 'CPU check' }]}
+                        onChange={e => {
+                          this.setState({ alert: e.target.value });
+                        }}
+                      />
+                    </EuiFormRow>
+                    <EuiFormRow label="Interval">
+                      <EuiFieldText
+                        name="interval"
+                        value={this.state.interval}
+                        onChange={e => {
+                          this.setState({ interval: e.target.value });
+                        }}
+                      />
+                    </EuiFormRow>
+                    <EuiFormRow label="Throttle">
+                      <EuiFieldText
+                        name="throttle"
+                        value={this.state.throttle}
+                        onChange={e => {
+                          this.setState({ throttle: e.target.value });
+                        }}
+                      />
+                    </EuiFormRow>
+                    <EuiFormRow label="Warning">
+                      <EuiRange
+                        min={0}
+                        max={100}
+                        name="warningThreshold"
+                        id="warningThreshold"
+                        value={this.state.warningThreshold}
+                        onChange={e => {
+                          this.setState({ warningThreshold: Number(e.target.value) });
+                        }}
+                        step={1}
+                        showLabels
+                        showValue
+                      />
+                    </EuiFormRow>
+                    <EuiFormRow>
+                      <EuiCheckboxGroup
+                        options={this.state.warningActions}
+                        idToSelectedMap={this.state.warningActionsMap}
+                        onChange={(optionId: any) => {
+                          this.setState({
+                            warningActionsMap: {
+                              ...this.state.warningActionsMap,
+                              [optionId]: !this.state.warningActionsMap[optionId],
+                            },
+                          });
+                        }}
+                      />
+                    </EuiFormRow>
+                    <EuiFormRow label="Severe">
+                      <EuiRange
+                        min={0}
+                        max={100}
+                        name="severeThreshold"
+                        id="severeThreshold"
+                        value={this.state.severeThreshold}
+                        onChange={e => {
+                          this.setState({ severeThreshold: Number(e.target.value) });
+                        }}
+                        step={1}
+                        showLabels
+                        showValue
+                      />
+                    </EuiFormRow>
+                    <EuiFormRow>
+                      <EuiCheckboxGroup
+                        options={this.state.severeActions}
+                        idToSelectedMap={this.state.severeActionsMap}
+                        onChange={(optionId: any) => {
+                          this.setState({
+                            severeActionsMap: {
+                              ...this.state.severeActionsMap,
+                              [optionId]: !this.state.severeActionsMap[optionId],
+                            },
+                          });
+                        }}
+                      />
+                    </EuiFormRow>
+                  </EuiForm>
+                </EuiModalBody>
+                <EuiModalFooter>
+                  <EuiButtonEmpty onClick={() => this.setState({ showModal: false })}>
+                    Cancel
+                  </EuiButtonEmpty>
+                  <EuiButton color="primary" fill={true} onClick={this.onSaveClick.bind(this)}>
+                    Save
+                  </EuiButton>
+                </EuiModalFooter>
+              </EuiModal>
+            </EuiOverlayMask>
+          )}
+        </EuiFlexGroup>
+      </Toolbar>
+    );
+  }
+}
+
+export const SnapshotToolbar = injectI18n(Component);
